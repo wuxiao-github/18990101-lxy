35e86ca91e0667b50688f6501dfcb8f59b139ced
wuxiao <wuxiao0063@163.com>
2020-11-07 23:53:34 +0800

    lxy

---
+++ Homework05/app/src/main/java/com/example/lxy/bean/UserInfo.java
@@ -0,0 +1,29 @@
+package com.example.lxy.bean;
+
+public class UserInfo {
+
+    public long rowid;
+    //public int sn;
+    public String name;
+
+    // public int age;
+    // public long height;
+    // public float weight;
+    // public boolean married;
+    public String update_time;
+    public String phonenumber;
+    public String phonepwd;
+    public String phone;
+    public String pwd;
+
+    public UserInfo() {
+        rowid = 0L;
+        name="";
+        update_time = "";
+        phone = "";
+        pwd = "";
+        phonenumber="";
+        phonepwd="";
+    }
+
+}
